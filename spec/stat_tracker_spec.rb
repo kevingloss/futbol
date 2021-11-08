0d37e7b690d8070bec8e0e70e049000be210ad65
Kevin Gloss <kgloss89@gmail.com>
2021-11-07 20:56:31 -0700

    Update testing for most/least accurate team

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -330,13 +330,14 @@ RSpec.describe StatTracker do
     end
   end
   describe ' #most_accurate_team' do
-    xit 'returns the name of the team with the best ratio of shots to goals for the season' do
-    expect(@stat_tracker.most_accurate_team('20122013')).to eq('FC Dallas')
+    it 'returns the name of the team with the best ratio of shots to goals for the season' do
+    expect(@stat_tracker.most_accurate_team('20122013')).to eq('DC United')
+    expect(@stat_tracker.most_accurate_team("20142015")).to eq("Toronto FC")
     end
   end
   describe ' #least_accurate_team' do
-    xit 'returns the name of the team with the worst ratio of shots to goals for the season' do
-    expect(@stat_tracker.least_accurate_team('20122013')).to eq('Houston Dynamo')
+    it 'returns the name of the team with the worst ratio of shots to goals for the season' do
+    expect(@stat_tracker.least_accurate_team('20122013')).to eq('New York City FC')
     end
   end
 
