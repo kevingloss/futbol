45ce4f9ae671371cfcf4aa209f7625e3a0f2ee37
Kevin Gloss <kgloss89@gmail.com>
2021-11-08 20:09:26 -0700

    Remove unnecessary attr_reader variables

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -21,14 +21,10 @@ RSpec.describe Game do
     it "checks that all the attributes" do
       expect(@game1.game_id).to eq('2012030221')
       expect(@game1.season).to eq('20122013')
-      expect(@game1.type).to eq('Postseason')
-      expect(@game1.date_time).to eq('5/16/13')
       expect(@game1.away_team_id).to eq('3')
       expect(@game1.home_team_id).to eq('6')
       expect(@game1.away_goals).to eq(2)
       expect(@game1.home_goals).to eq(3)
-      expect(@game1.venue).to eq('Toyota Stadium')
-      expect(@game1.venue_link).to eq('/api/v1/venues/null')
     end
   end
 
